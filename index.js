219e3eab4872bdba760bbe19bdf4b63627b07779
支海振 <zhihaizhen@zhihaizhendeMacBook-Pro.local>
2023-11-13 18:32:34 +0800

    feat: add test button

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * joyin-ui
+ * joyin组件库
+ */
+
+import IelPagination from './lib/iel/pagination';
+import IelTableLayout from './lib/iel/tableLayout';
+
+export {
+  Login,
+  FrameIndex, FrameHome,
+  Approval,
+  Preview,
+  IelPagination,
+  IelTableLayout,
+  i18n,
+  directives,
+};
