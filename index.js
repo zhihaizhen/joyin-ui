4df7bec5fa984a209c15c5911093d4a84ef8a92d
zhihaihen <1150305954@qq.com>
2023-11-16 11:01:32 +0800

    fix: dateRange update

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  * joyin组件库
  */
 
-import JoyinButton from './lib/plugins/button';
 import JoyinRangePicker from './lib/plugins/rangePicker';
 export {
-  JoyinButton,
   JoyinRangePicker
 };
--- lib/plugins/rangePicker/src/main.vue
+++ lib/plugins/rangePicker/src/main.vue
@@ -1,42 +1,25 @@
-/* eslint-disable */
-<template>
-  <div class="joyin-range-picker-container">
-    <div class="main-container" @click.stop="onContainerClick">
-      <div>
-        <span>{{ rangeDate[0] ? rangeDate[0] : placeholder[0] }}</span>
-        <a-icon v-if="isClearStart && rangeDate[0]" type="close-circle" @click.stop="onClearStart"/>
-      </div>
-      <i>-</i>
-      <div>
-        <span>{{ rangeDate[1] ? rangeDate[1] : placeholder[1] }}</span>
-        <a-icon v-if="isClearEnd && rangeDate[1]" type="close-circle" @click.stop="onClearEnd" />
-      </div> 
-    </div>
-    <a-range-picker
-      v-model="rangeDate"
-      v-bind="$attrs"
-      :open="isOpen"
-      :format="format"
-      :valueFormat="format"
-      @change="onDateChange"
-    >
-    </a-range-picker>
-  </div>
-</template>
 <script>
 export default {
   name: 'JoyinRangePicker',
   props: {
     value: [],
+    // 日期格式
     format: {
       type: String,
       default: 'YYYY-MM-DD'
     },
-    isClearStart: {
+    // 清除开始日期
+    clearStart: {
       type: Boolean,
       default: true
     },
-    isClearEnd: {
+    // 清除结束日期
+    clearEnd: {
+      type: Boolean,
+      default: true
+    },
+    // 清除所有
+    clearAll: {
       type: Boolean,
       default: true
     },
@@ -57,6 +40,10 @@ export default {
   watch: {
     value: {
       handler(nVal) {
+        if (!Array.isArray(nVal)) {
+          this.rangeDate = [];
+          return;
+        }
         const [s, e] = nVal;
         const startDate = typeof s === 'object' && s.isValid() ? s.format(this.format) : s;
         const endDate = typeof e === 'object' && e.isValid() ? e.format(this.format) : e;
@@ -76,27 +63,87 @@ export default {
     })
   },
   methods: {
-    onClearStart() {
-      const [, e] = this.rangeDate;
-      this.rangeDate = ['', e];
-      this.$emit('input', ['', e]);
-      this.$emit('change', ['', e]);
+    onClearByIndex(i) {
+      let changeDate = [];
+      if (!this.clearAll) {
+        const [s, e] = this.rangeDate;
+        changeDate = i === 0 ? ['', e] : [s, ''];
+      }
+      this.rangeDate = changeDate;
+      this.$emit('input', changeDate);
+      this.$emit('change', changeDate);
     },
-    onClearEnd() {
-      const [s] = this.rangeDate;
-      this.rangeDate = [s, ''];
-      this.$emit('input', [s, '']);
-      this.$emit('change', [s, '']);
-    },
+
     onDateChange(val) {
       this.isOpen = false;
       const [s, e] = val;
       this.$emit('input', [s, e]);
       this.$emit('change', val);
     },
+
     onContainerClick() {
       this.isOpen = true;
+    },
+
+    renderItem(h, i) {
+      const spanEl = h(
+        'span', 
+        { class: !this.rangeDate[i] && 'placeholder'}, 
+        this.rangeDate[i] ? this.rangeDate[i] : this.placeholder[i]
+      );
+      const iconEl = this.clearStart && this.rangeDate[i] && 
+        h('a-icon', 
+          { 
+            attrs: {
+              type: 'close-circle'
+            },
+            on: {
+              click: e => {
+                e && e.stopPropagation();
+                this.onClearByIndex(i);
+              }
+            }
+          });
+      return h('div', null, [spanEl, this.clearAll && !i ? '' : iconEl]);
+    },
+
+    renderMain(h) {
+      return h('div', 
+        {
+          class: 'main-container',
+          on: {
+            click: e => {
+              e && e.stopPropagation();
+              this.onContainerClick();
+            }
+          }
+        }, 
+        [this.renderItem(h, 0), h('i',null,'~'), this.renderItem(h, 1)]);
+    },
+
+    renderDate(h) {
+      return h('a-range-picker',
+      {
+        attrs: {
+          ...this.$attrs,
+          value: this.value,
+          fomat: this.format,
+          valueFormat: this.format,
+          open: this.isOpen
+        },
+        on: {
+          change: e => {
+            this.onDateChange(e);
+          },
+          ok: e => {
+            this.$emit('ok', e);
+          }
+        }
+      })
     }
+  },
+  render(h) {
+    return h('div', { class: 'joyin-range-picker-container' }, [this.renderMain(h), this.renderDate(h)]);
   }
 }
 </script>
@@ -118,6 +165,7 @@ export default {
       align-items: center;
       justify-content: space-between;
       padding: 0 10px;
+      color: #242933;
       >div{
         >span {
           margin-right: 5px;
@@ -125,11 +173,18 @@ export default {
         }
         >i {
           display: none;
+          color: #bbbbbb;
+          &:hover {
+            color: #242933;
+          }
+        }
+        .placeholder{
+          color: #bbbbbb;
         }
       }
       &:hover {
         i {
-          display: inline;
+          display: inline !important;
         }
       }
     }
